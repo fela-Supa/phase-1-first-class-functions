8664c375613f5c1ba1df88acb7db5a5236dc7bd6
fela-Supa <alvinaiterra@gmail.com>
2022-12-03 13:18:37 +0300

    Completed

---
+++ index.js
@@ -0,0 +1,12 @@
+function exerciseRoutine(postRunActivity) {
+    runFiveMiles();
+    postRunActivity();
+}
+
+return function (){
+    console.log(`Nom nom nom, this ${breakfast} is delicious!`);
+};
+
+let show = function(){
+    console.log ('Anonymous Function')
+}
